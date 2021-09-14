6b3fefc691748656c680a1a17d569159fd6dee84
Suneil England <70965512+adastra-react@users.noreply.github.com>
2021-09-14 18:28:30 -0500

    initial commit

--- App.js
+++ App.js
@@ -5,13 +5,40 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.carContainer} >
+          <View style={styles.titles}>
+             <Text style={styles.title} >Model S</Text>
+             <Text style={styles.subTitle} >Starting at $69000</Text>
+          </View>
+      </View>
+        
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  titles: {
+    marginTop: '30%',
+    width: '100%',
+    alignItems: 'center'
+  },
+
+  title : {
+    fontSize: 40,
+    fontWeight: '600'
+  },
+
+  subTitle : {
+    
+  },
+
+  carContainer: {
+    width: '100%',
+    height: '100%'
+  },
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
