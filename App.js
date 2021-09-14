d921a80d2e7e0f9860ac4e62f5572a54f2eb41b8
Suneil England <70965512+adastra-react@users.noreply.github.com>
2021-09-14 18:44:58 -0500

    first page with tesla model s added

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.carContainer} >
+
+      <ImageBackground
+        source={require('./assets/images/ModelX.jpeg')}
+        style={styles.image}
+      />
+      
           <View style={styles.titles}>
              <Text style={styles.title} >Model S</Text>
              <Text style={styles.subTitle} >Starting at $69000</Text>
           </View>
       </View>
-        
 
       <StatusBar style="auto" />
     </View>
@@ -25,13 +30,21 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
 
+  image: {
+    height: '100%',
+    width: '100%',
+    resizeMode: 'cover',
+    position: 'absolute'
+  },
+
   title : {
     fontSize: 40,
     fontWeight: '600'
   },
 
   subTitle : {
-    
+    fontSize: 16,
+    color: '#5c5e62'
   },
 
   carContainer: {
