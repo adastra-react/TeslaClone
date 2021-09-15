c25da934b82800c8f635921894bfd7c3cb5090b5
Suneil England <70965512+adastra-react@users.noreply.github.com>
2021-09-14 19:12:30 -0500

    car item component added

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import CarItem from './components/CarItem/CarItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.carContainer} >
 
-      <ImageBackground
-        source={require('./assets/images/ModelX.jpeg')}
-        style={styles.image}
-      />
-      
-          <View style={styles.titles}>
-             <Text style={styles.title} >Model S</Text>
-             <Text style={styles.subTitle} >Starting at $69000</Text>
-          </View>
-      </View>
+      <CarItem/>
 
       <StatusBar style="auto" />
     </View>
@@ -24,33 +15,7 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  titles: {
-    marginTop: '30%',
-    width: '100%',
-    alignItems: 'center'
-  },
-
-  image: {
-    height: '100%',
-    width: '100%',
-    resizeMode: 'cover',
-    position: 'absolute'
-  },
-
-  title : {
-    fontSize: 40,
-    fontWeight: '600'
-  },
-
-  subTitle : {
-    fontSize: 16,
-    color: '#5c5e62'
-  },
-
-  carContainer: {
-    width: '100%',
-    height: '100%'
-  },
+  
 
   container: {
     flex: 1,
---
+++ components/CarItem/styles.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    titles: {
+      marginTop: '30%',
+      width: '100%',
+      alignItems: 'center'
+    },
+  
+    image: {
+      height: '100%',
+      width: '100%',
+      resizeMode: 'cover',
+      position: 'absolute'
+    },
+  
+    title : {
+      fontSize: 40,
+      fontWeight: '600'
+    },
+  
+    subTitle : {
+      fontSize: 16,
+      color: '#5c5e62'
+    },
+  
+    carContainer: {
+      width: '100%',
+      height: '100%'
+    }
+  });
+
+  export default styles;
